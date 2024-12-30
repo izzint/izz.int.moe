d5d7cffb79db915151864469937bc63b611d9d68
izz <44090193+izzint@users.noreply.github.com>
2024-12-30 18:27:26 -0500

    some visual changes

--- src/static/js/nekostats.js
+++ src/static/js/nekostats.js
@@ -1,9 +1,5 @@
-/*
-this script is free to use for anything, whenever!
-by izz
-*/
-
 const USERNAME = "izz";
+let devMode = false;
 
 // gets the data from the nekoweb api!!
 const getStats = async () => {
@@ -12,9 +8,11 @@ const getStats = async () => {
     const created = new Date(stats.created_at).toLocaleDateString();
     const updated = new Date(stats.updated_at).toLocaleDateString();
     const updatedTime = new Date(stats.updated_at).toLocaleTimeString();
-
     const updateDays = Math.floor(stats.created_at/8.64e7); // hacky hack
+
+    document.getElementById("hits").innerText = stats.views;
+    document.getElementById("followers").innerText = stats.followers;
 }
-if (!devMode == true) {
-    getStats();
+if (!devMode) {
+getStats();
 }
